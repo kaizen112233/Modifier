640891f8ad82acc51ae881e71c09475fc85d15a8
Afghan Propleyer <156155247+kaizen112233@users.noreply.github.com>
2024-01-30 14:02:15 +0700

    Add files via upload

---
+++ Modifier.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modifier;
+
+/**
+ *
+ * @author MOKLET006
+ */
+public class Modifier {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
